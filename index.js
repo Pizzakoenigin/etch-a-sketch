4e76a5e6364e2125e3efcf682d631b98621bec2a
Franziska <Franziska.Roder@gmx.de>
2025-02-05 16:01:22 +0100

    createGrid and square creation works

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ function init() {
     container.classList.add('container')
     document.querySelector('body').appendChild(container)
 
-    buildGrid(64)
+    buildGrid()
     eventListener()
 }
 
-function buildGrid(amount) {
+function buildGrid() {
     let container = document.querySelector('.container')
+    let containerSize = 720
+    let squaresPerSide = 100
+    let squaresTotal = squaresPerSide * squaresPerSide
+    let size = containerSize/squaresPerSide
+
+    container.style.width = `${containerSize}px`
+    container.style.height = `${containerSize}px`
+    container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`
     
-    for (let i = 1; i <= amount; i++) {
+    for (let i = 1; i <= squaresTotal; i++) {
         let square = document.createElement('div');
+        // square.style.width = `${size}px`
+        // square.style.height = `${size}px`        
         container.appendChild(square)
         square.classList.add('square')
+
     }
+
+  
     
     
 }
@@ -26,8 +39,9 @@ function buildGrid(amount) {
 function eventListener() {
     let squares = document.querySelectorAll('.square');
     squares.forEach((square) => (
+        
         square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = 'red'
+            square.style.backgroundColor = 'rgba(0,0,0,0.1)'
         })
     ))
 }
